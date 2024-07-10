3da050d619dcb9e4961f7ba2bbb8aeffda22ee88
SeokhunEom <tommya98@naver.com>
2024-07-10 23:47:55 +0900

    6장: 컴포넌트 반복

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,11 @@
 import { Component } from 'react';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
     return (
       <>
-        <ValidationSample />
-        <div>
-          <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-          <button onClick={() => this.scrollBox.scrollBottom()}>
-            맨 밑으로
-          </button>
-        </div>
+        <IterationSample />
       </>
     );
   }

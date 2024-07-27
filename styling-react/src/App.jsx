4c23d9c792958c62d89dc16f9e53d8cd3e99eafe
SeokhunEom <tommya98@naver.com>
2024-07-27 19:04:51 +0900

    9장: 컴포넌트 스타일링

---
+++ styling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import CSSModule from './CSSModule';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponents';
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+      <CSSModule />
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;

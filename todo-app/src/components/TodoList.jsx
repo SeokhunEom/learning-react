47b0d327f08dfd79f56b1f0a428fb9d78c336ba4
SeokHun <tommya98@naver.com>
2024-08-15 13:59:46 +0900

    11장: 컴포넌트 성능 최적화

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,22 +1,39 @@
 import './TodoList.scss';
 
+import React, { useCallback } from 'react';
+
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListitem';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
+
   return (
-    <div className="TodoList">
-      {todos.map((todo) => {
-        return (
-          <TodoListItem
-            key={todo.id}
-            todo={todo}
-            onRemove={onRemove}
-            onToggle={onToggle}
-          />
-        );
-      })}
-    </div>
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
